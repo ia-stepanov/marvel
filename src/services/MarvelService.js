a3ab60b7763401320296c9f93d6bc90163a6dd31
Igor Stepanov <igorstepanov@hotmail.com>
2022-06-23 19:34:46 +0300

    build(Marvel): трансформировать данные, реализовать отрисовку компонента со случайным персонажем

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,14 +12,30 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const res = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
     );
+    return res.data.results.map(this._transformCharacter);
   };
 
-  getCharacter = (id) => {
-    return this.getResource(`${this._apiBase}characters/${id}?&${this._apiKey}`);
+  getCharacter = async (id) => {
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?&${this._apiKey}`
+    );
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description
+        ? `${char.description.slice(0, 210)}…`
+        : 'There is no description for this character',
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
   };
 }
 
