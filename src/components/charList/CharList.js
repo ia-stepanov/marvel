9f29b343f4545f548f32d14cfda25dadf6fcc954
Igor Stepanov <igorstepanov@hotmail.com>
2022-06-24 12:04:53 +0300

    build(Marvel): реализовать логику работы компонента charList

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,85 @@
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import { Component } from 'react';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import MarvelService from '../../services/MarvelService';
 
-const CharList = () => {
-  return (
-    <div className="char__list">
+import './charList.scss';
+
+class CharList extends Component {
+  state = {
+    charList: [],
+    loading: true,
+    error: false,
+  };
+
+  marvelService = new MarvelService();
+
+  componentDidMount() {
+    //  prettier-ignore
+    this.marvelService
+      .getAllCharacters()
+      .then(this.onCharListLoaded)
+      .catch(this.onError);
+  }
+
+  onCharListLoaded = (charList) => {
+    this.setState({
+      charList,
+      loading: false,
+    });
+  };
+
+  onError = () => {
+    this.setState({
+      loading: false,
+      error: true,
+    });
+  };
+
+  renderItems(arr) {
+    const items = arr.map((item) => {
+      let imgStyle = { objectFit: 'cover' };
+      if (
+        item.thumbnail ===
+        'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+      ) {
+        imgStyle = { objectFit: 'unset' };
+      }
+
+      return (
+        <li className="char__item" key={item.id}>
+          <img src={item.thumbnail} alt={item.name} style={imgStyle} />
+          <div className="char__name">{item.name}</div>
+        </li>
+      );
+    });
+
+    return (
+      //  prettier-ignore
       <ul className="char__grid">
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item char__item_selected">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
+        {items}
       </ul>
-      <button className="button button__main button__long">
-        <div className="inner">load more</div>
-      </button>
-    </div>
-  );
-};
+    );
+  }
+
+  render() {
+    const { charList, loading, error } = this.state;
+    const items = this.renderItems(charList);
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? items : null;
+
+    return (
+      <div className="char__list">
+        {errorMessage}
+        {spinner}
+        {content}
+        <button className="button button__main button__long">
+          <div className="inner">load more</div>
+        </button>
+      </div>
+    );
+  }
+}
 
 export default CharList;
